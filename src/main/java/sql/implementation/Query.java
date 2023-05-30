981cf06d7bcd587fc075a957bfef8c1bd1f23d66
irinatomic <irina.z.tomic@gmail.com>
2023-05-30 20:03:46 +0200

    Parsing query to SQL object (try 2)

--- src/main/java/sql/implementation/Query.java
+++ src/main/java/sql/implementation/Query.java
@@ -1,9 +1,16 @@
 package sql.implementation;
 
 import lombok.Getter;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectItem;
 import sql.composite.Token;
 import sql.composite.TokenComposite;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,6 +23,34 @@ public class Query extends TokenComposite {
 
     @Override
     public void parseQueryToSQLObject(String query) {
+
+        try {
+            // Parse the SQL query
+            Statement statement = CCJSqlParserUtil.parse(query);
+
+            // Check if the statement is a Select statement
+            if (statement instanceof Select) {
+                Select selectStatement = (Select) statement;
+
+                // Get the select body
+                PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
+
+                // Get the select items
+                List<SelectItem> selectItems = plainSelect.getSelectItems();
+
+                // Extract and print the select clause
+                StringBuilder selectClause = new StringBuilder();
+                for (SelectItem selectItem : selectItems) {
+                    selectClause.append(selectItem.toString()).append(", ");
+                }
+                // Remove the trailing comma and space
+                selectClause.setLength(selectClause.length() - 2);
+
+                System.out.println("SELECT clause: " + selectClause);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         // Define regular expressions for each clause
         // Pretpostavka da su po redu navedene naredbe
@@ -35,12 +70,12 @@ public class Query extends TokenComposite {
         String orderByClause = extractClause(query, orderByRegex);
 
         //print
-        System.out.println("SELECT clause: " + selectClause.trim());
-        System.out.println("FROM clause: " + fromClause.trim());
-        System.out.println("WHERE clause: " + whereClause.trim());
-        System.out.println("GROUP BY clause: " + groupByClause.trim());
-        System.out.println("HAVING clause: " + havingClause.trim());
-        System.out.println("ORDER BY clause: " + orderByClause.trim());
+//        System.out.println("SELECT clause: " + selectClause.trim());
+//        System.out.println("FROM clause: " + fromClause.trim());
+//        System.out.println("WHERE clause: " + whereClause.trim());
+//        System.out.println("GROUP BY clause: " + groupByClause.trim());
+//        System.out.println("HAVING clause: " + havingClause.trim());
+//        System.out.println("ORDER BY clause: " + orderByClause.trim());
 
     }
 
